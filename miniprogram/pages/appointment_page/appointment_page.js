67eaabbb351b2dd8599393acada98336d7fee456
linxueni <2849773560@qq.com>
2021-02-07 10:51:20 +0800

    lxn
    lxn

--- miniprogram/pages/appointment_page/appointment_page.js
+++ miniprogram/pages/appointment_page/appointment_page.js
@@ -1,13 +1,51 @@
 // miniprogram/pages/appointment_page/appointment_page.js
+var idinfolist = [
+  { "code": "9:00-10:10", "text": '' },
+  { "code": "10:20-11:05", "text": '' },
+  { "code": "11:15-12:00", "text": '' },
+  { "code": "12:00-13:00", "text": ''},
+  { "code": "13:00-13:45", "text": ''},
+  { "code": "13:55-14:40", "text": ''},
+  { "code": "14:50-15:35", "text": ''},
+  { "code": "15:45-16:30", "text": ''},
+  { "code": "16:40-17:25", "text": ''},
+  { "code": "17:25-18:30", "text": ''},
+  { "code": "18:30-19:15", "text": ''},
+  { "code": "19:25-20:10", "text": ''},
+  { "code": "20:20-21:05", "text": ''},
+  { "code": "21:05-22:00", "text": ''},
+  { "code": "22:00-23:00", "text": ''},
+  { "code": "23:00-24:00", "text": ''}
+ ]
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    listData: idinfolist,  
+    inputValue: '', //用于显示输入语句
+    searchinput: '', //用户输入的查询语句
+    shows: false, //控制下拉列表的显示隐藏，false隐藏、true显示
+    selectDatas: ['2021-2-5(周五)', '2021-2-6(周六)', '2021-2-5(周日)'], //下拉列表的数据
+    indexs: 0, //选择的下拉列 表下标,
+   },
+  // 点击下拉显示框
+  selectTaps() {
+    this.setData({
+      shows: !this.data.shows,
+    });
+  },
+  // 点击下拉列表
+  optionTaps(e) {
+    let Indexs = e.currentTarget.dataset.index; //获取点击的下拉列表的下标
+    console.log(Indexs)
+    this.setData({
+      indexs: Indexs,
+      shows: !this.data.shows
+    });
 
   },
-
   /**
    * 生命周期函数--监听页面加载
    */
--- miniprogram/pages/room_overview/room_overview.js
+++ miniprogram/pages/room_overview/room_overview.js
@@ -1,12 +1,32 @@
 // miniprogram/pages/room_overview/room_overview.js
+var idinfolist = [
+  { "code": "9:00-10:10", "text": '' },
+  { "code": "10:20-11:05", "text": '' },
+  { "code": "11:15-12:00", "text": '' },
+  { "code": "12:00-13:00", "text": ''},
+  { "code": "13:00-13:45", "text": ''},
+  { "code": "13:55-14:40", "text": ''},
+  { "code": "14:50-15:35", "text": ''},
+  { "code": "15:45-16:30", "text": ''},
+  { "code": "16:40-17:25", "text": ''},
+  { "code": "17:25-18:30", "text": ''},
+  { "code": "18:30-19:15", "text": ''},
+  { "code": "19:25-20:10", "text": ''},
+  { "code": "20:20-21:05", "text": ''},
+  { "code": "21:05-22:00", "text": ''},
+  { "code": "22:00-23:00", "text": ''},
+  { "code": "23:00-24:00", "text": ''}
+ ]
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
-  },
+    listData: idinfolist,  
+    inputValue: '', //用于显示输入语句
+    searchinput: '', //用户输入的查询语句
+   },
 
   /**
    * 生命周期函数--监听页面加载
